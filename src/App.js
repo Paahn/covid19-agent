812038d5b39603a089e7798516d7c6f72af137ac
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-01 12:57:23 -0800

    added comments for the wireframe

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="app">
       <h1>Covid 19 Agent</h1>
+      {/* Header */}
+      {/* Title + select input dropdown field */}
+      {/* Infobox*/}
+      {/* Infobox*/}
+      {/* Infobox*/}
+      {/* Infobox*/}
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
