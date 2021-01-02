14f260542ac0cc478703bf877579c488e9dbfaf6
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-01 16:51:06 -0800

    added styling to dropdown

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Covid 19 Agent</h1>
-      <FormControl className="app__dropdown">
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="Worldwide">Worldwide</MenuItem>
-          <MenuItem value="Worldwide">1</MenuItem>
-          <MenuItem value="Worldwide">2</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>Covid 19 Agent</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            <MenuItem value="Worldwide">1</MenuItem>
+            <MenuItem value="Worldwide">2</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
       {/* Header */}
       {/* Title + select input dropdown field */}
       {/* Infobox*/}
