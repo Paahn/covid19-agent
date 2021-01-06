6fd9dd4e6a0ebc23f8a25edc3d6fa6aa16aeada9
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-06 14:33:52 -0800

    passed an extra variable to differentiate between the 3 types of case that we may have from the API, cases, deaths and recovered

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -4,30 +4,33 @@ import { Line } from "react-chartjs-2";
 function LineGraph() {
   const [data, setData] = useState({});
 
+  const buildChartData = (data, caseType) => {
+    const chartData = [];
+    let lastDataPoint;
+
+    data[caseType].forEach(date => {
+      if (lastDataPoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[caseType][date] - lastDataPoint
+        }
+        chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[caseType][date];
+    });
+    return chartData;
+  }
+
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
     .then((response) => response.json())
     .then ((data) => {
       console.log("LINEGRAPH DATA ", data);
+      const chartData = buildChartData(data);
+      setData(chartData);
     })
   }, []);
 
-  const buildChartData = data => {
-    const chartData = [];
-    let lastDataPoint;
-
-    data.cases.forEach(date => {
-      if (lastDataPoint) {
-        const newDataPoint = {
-          x: date,
-          y: data['cases'][date] - lastDataPoint
-        }
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data['cases'][date];
-    });
-    return chartData;
-  }
 
   return (
     <div>
