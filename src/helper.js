6e346c81ddb8c0486bdc673373afeddced12af69
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-06 17:33:28 -0800

    implement helper function to draw a circle on the map for the selected country/countries with the right data

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,44 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+  cases: {
+    hex: "#CC1034",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: "#7dd71d",
+    multiplier: 1200,
+  },
+  deaths: {
+    hex: "#fb4443",
+    multiplier: 2000,
+  },
+};
+
 export const sortData = (data) => {
   const sortedData = [...data];
 
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
-}
+};
+
+// Draw a circle on the map to indicate data volume (for example number of cases) by
+// using an interactive tooltip
+export const showDataOnMap = (data, caseType='cases') => (
+  data.map(country => (
+    <Circle
+      center={[data.countryInfo.lat, data.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[caseType].hex}
+      fillColor={casesTypeColors[caseType].hex}
+      radius={
+        Math.sqrt(country[caseType]) * casesTypeColors[caseType].multiplier
+      }
+    >
+      <Popup>
+        <h1>Hello there!</h1>
+      </Popup>
+    </Circle>
+  ))
+);
