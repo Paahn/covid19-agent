c53f6f57314fe30137802a160ff1cc3d9addb7f2
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-06 16:20:14 -0800

    installed map package and laid down the structure for the map

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
 
 function Map() {
   return (
     <div className="map">
-      <h1>MAP GOES HERE</h1>
+      <LeafletMap>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LeafletMap>
     </div>
   )
 }
