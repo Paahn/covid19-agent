d89782fed9ee80735303852a3762efeb2b4b0c83
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-07 17:58:12 -0800

    changed color of recovered text in infobox

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -12,7 +12,7 @@ function InfoBox({ active, isRed, title, cases, total, ...props }) {
         <Typography className="infobox__title" color="textSecondary">
           {title}
         </Typography>
-        <h2 className="infobox__cases">{cases}</h2>
+        <h2 className={`infobox__cases ${!isRed && "infobox__cases--green"}`}>{cases}</h2>
         <Typography className="infobox__total" color="textSecondary">
           {total} Total
         </Typography>
